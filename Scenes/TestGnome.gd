275c12d9a9ee9b69a0837b14457621abf05bdc96
Carlos Michea A <72899324+CarlosMichea@users.noreply.github.com>
2024-07-17 19:31:09 -0400

    Movement Basic
    No Physics

---
+++ Scenes/TestGnome.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var speed := 500
+
+var lookLeft := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#Input direction to movement
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	position += direction * speed * delta
+	
+	# Sprite Flip
+	if direction.x <= -0.15:
+		$TestGnomeImage.flip_h = true
+		lookLeft = true
+	if direction.x >= 0.15:
+		$TestGnomeImage.flip_h = false
+		lookLeft = false
+		
+		
