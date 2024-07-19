1798dc047df6aa6368506d8f7fcfcd31f6aad3e5
Carlos Michea A. <72899324+CarlosMichea@users.noreply.github.com>
2024-07-18 22:43:10 -0400

    Mov Enemy 2.0 (sin patrulla)
    Linea de vision y cambio de estatus si pierden vision mucho rato

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -3,21 +3,46 @@ extends CharacterBody2D
 const speed = 35
 
 var playerIn: bool = false
+var playerSeen: bool = false
 var chasing: bool = false
 
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@onready var ray_cast = $RayCast2D
 
 func _ready():
-	make_path()
+	
+	pass
 	
 
 func _physics_process(_delta: float) -> void:
+	
+	#Raycast
+	if playerIn == true:
+		var space = get_viewport().world_2d.direct_space_state
+		var result = space.intersect_ray(PhysicsRayQueryParameters2D.create(global_transform.origin, player.global_transform.origin))
+		
+		if result.collider == player:
+			if playerSeen == false:
+				playerSeen = true
+				print("heyho")
+				
+				chasing = true
+				$Timer2.stop()
+				$Timer.start()
+		else:
+			if playerSeen == true:
+				playerSeen = false
+				$Timer2.start()
+				print("nah")
+	
 	if chasing == true:
-		# Get path and move
+		# Chasing movement
 		var dir = to_local(nav_agent.get_next_path_position()).normalized()
 		velocity = dir * speed
 		move_and_slide()
+	else:
+		$Timer.stop()
 		
 
 func make_path() -> void:
@@ -30,20 +55,26 @@ func _on_timer_timeout():
 
 func _on_area_2d_body_entered(body):
 	if body == player:
-		$Timer2.stop()
 		playerIn = true
-		chasing = true
-		print(body)
+		print("entered")
+		
+		if playerSeen == true:
+			chasing = true
+			$Timer2.stop()
+			$Timer.start()
 		
 
 func _on_area_2d_body_exited(body):
+	
 	if body == player:
+		playerIn = false
 		$Timer2.start()
-		print(body)
+		print("exited")
 		
 
 func _on_timer_2_timeout():
 	playerIn = false
 	chasing = false
+	playerSeen = false
 	print("escaped")
 	
