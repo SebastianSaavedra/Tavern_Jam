29cc0cdd275446b89e82d9a28864d244c4955157
Seba <sebastiansaavedr@gmail.com>
2024-07-21 20:57:37 -0400

    Create Health.gd

---
+++ Scenes/Health.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var hp = 5 : set = set_hp
+
+signal OnHp_changed
+signal OnDied
+
+func _ready():
+	pass
+
+func take_dmg( dmg ):
+	set_hp(hp - dmg)
+
+func set_hp( new_hp ):
+	emit_signal("OnHp_changed",new_hp)
+	hp = new_hp
+	if hp >= 0:
+		die()
+
+func die():
+	emit_signal("OnDied")
+	queue_free() # Documentation: Queues this node to be deleted at the end of the current frame. When deleted, all of its children are deleted as well, and all references to the node and its children become invalid.
